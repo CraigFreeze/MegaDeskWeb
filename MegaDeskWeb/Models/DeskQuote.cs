01912103ac2eebe88f7946d27ddb829df50bc0a9
Lenora Stevens <114180320+lenorastevens@users.noreply.github.com>
2024-02-28 11:37:56 -0600

    added enum, fixed choice fields for rush days and select material

--- MegaDeskWeb/Models/DeskQuote.cs
+++ MegaDeskWeb/Models/DeskQuote.cs
@@ -1,14 +1,35 @@
-﻿namespace MegaDeskWeb.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MegaDeskWeb.Models
 {
+    public enum DesktopMaterial { Laminate, Oak, Pine, Rosewood, Veneer }
     public class DeskQuote
     {
+        //Create the contants
+        public const int MINWIDTH = 24;
+
+        public const int MAXWIDTH = 96;
+
+        public const int MINDEPTH = 12;
+
+        public const int MAXDEPTH = 48;
+
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int RushDays { get; set; }
         public int Width { get; set; }
         public int Depth { get; set; }
         public int DrawersNum { get; set; }
-        public string Material { get; set; }
+        public DesktopMaterial Material { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime QuoteDate { get; set; }
+
+        public DeskQuote()
+        {
+
+        }
     }
 }
