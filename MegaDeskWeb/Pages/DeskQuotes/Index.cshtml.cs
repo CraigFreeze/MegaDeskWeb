3a0da3a9d3b6cce97a08c1fe77b20b3be5595621
Sylvie Jovet <sylviejovet@hotmail.com>
2024-02-28 23:16:56 -0700

    Added Sorting and Search by name. Completed styling.

--- MegaDeskWeb/Pages/DeskQuotes/Index.cshtml.cs
+++ MegaDeskWeb/Pages/DeskQuotes/Index.cshtml.cs
@@ -7,12 +7,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDeskWeb.Data;
 using MegaDeskWeb.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MegaDeskWeb.Pages.DeskQuotes
 {
     public class IndexModel : PageModel
     {
         private readonly MegaDeskWeb.Data.MegaDeskWebContext _context;
+       
 
         public IndexModel(MegaDeskWeb.Data.MegaDeskWebContext context)
         {
@@ -21,9 +23,57 @@ namespace MegaDeskWeb.Pages.DeskQuotes
 
         public IList<DeskQuote> DeskQuote { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? NameSort { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? DateSort { get; set; }
+
+        public async Task OnGetAsync(string sortOrder)
         {
-            DeskQuote = await _context.DeskQuote.ToListAsync();
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            IQueryable<DeskQuote> quotes = from q in _context.DeskQuote
+                                            orderby q.Name
+                                            select q;
+
+
+            if (!string.IsNullOrEmpty(SearchString) )
+            {
+                quotes = quotes.Where(q => q.Name.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(NameSort))
+            {
+                quotes = quotes.OrderByDescending(q => q.Name);
+            }
+
+            if (!string.IsNullOrEmpty(DateSort))
+            {
+                quotes = quotes.OrderByDescending(q => q.QuoteDate);
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    quotes = quotes.OrderByDescending(q => q.Name);
+                    break;
+                case "Date":
+                    quotes = quotes.OrderBy(q => q.QuoteDate);
+                    break;
+                case "date_desc":
+                    quotes = quotes.OrderByDescending(q => q.QuoteDate);
+                    break;
+                default:
+                    quotes = quotes.OrderBy(q => q.Name);
+                    break;
+            }
+
+            DeskQuote = await quotes.ToListAsync();
         }
     }
 }
